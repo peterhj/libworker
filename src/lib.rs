6614584d1ff05f325a34b38d38cd6df5c1952c06
Peter Jin <peterhaijin@gmail.com>
2016-04-29 22:11:15 -0700

    Updated naming conventions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,33 @@
+/*pub trait WorkerBuilder: Send + Clone {
+  type Worker;
+  type Context;
+
+  fn into_worker(self, context: Self::Context) -> Self::Worker;
+}*/
+
+#[derive(Clone, Copy)]
 pub struct WorkerData {
+  worker_rank:  usize,
   num_workers:  usize,
-  tid:  usize,
 }
 
 impl WorkerData {
-  pub fn new(num_workers: usize, tid: usize) -> WorkerData {
+  pub fn new(worker_rank: usize, num_workers: usize) -> WorkerData {
     WorkerData{
+      worker_rank:  worker_rank,
       num_workers:  num_workers,
-      tid:  tid,
     }
+  }
+
+  pub fn tid(&self) -> usize {
+    self.worker_rank
+  }
+
+  pub fn worker_rank(&self) -> usize {
+    self.worker_rank
   }
 
   pub fn num_workers(&self) -> usize {
     self.num_workers
-  }
-
-  pub fn tid(&self) -> usize {
-    self.tid
   }
 }
